5ac31ba46713426fb01cb95193537a6401a381b2
Sarah Schwarz <sarahschwarza@gmail.com>
2024-12-17 11:25:03 -0800

    Update simplemain.py

--- simplemain.py
+++ simplemain.py
@@ -17,7 +17,8 @@ VAS   = np.array([0, 50, 100, 150]) # Voltages on CH1 Tension
 VBS   = np.array([25, 25, 25, 25])    # Zeros on CH2 Compression
 FIELD = (-90_000, 90_000, 50)  # -9T to 9T at 50 Oe/sec?
 
-QD_FILES = r"C:/Users/sysadmin/Desktop/Razorbill-WilsonGroup/Sarah/"
+#QD_FILES = r"C:/Users/sysadmin/Desktop/Razorbill-WilsonGroup/Sarah/"
+QD_FILES = r"C:/Users/dynacool2 user/Desktop/Razorbill-WilsonGroup/Sarah"
 QD_FILE = max(glob.glob(QD_FILES+"*.dat"), key=os.path.getctime)
 print("Using File:", QD_FILE)
 
