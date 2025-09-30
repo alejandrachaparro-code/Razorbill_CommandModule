1daa1602cd333ac11b0096fc3cadd305ddc6cf26
Sarah Schwarz <sarahschwarza@gmail.com>
2025-09-30 10:42:50 -0700

    Update checkResources.py

--- checkResources.py
+++ checkResources.py
@@ -8,12 +8,13 @@ print(rm.list_resources())
 
 
 
-inst = rm.open_resource('ASRL1::INSTR')
+inst = rm.open_resource('ASRL8::INSTR')
 insta = rm.open_resource("GPIB0::28::INSTR")
 
 print("razorbill:")
 print(inst.query("*IDN?"))
 print("cap bridge:")
 print(insta.query("*IDN?"))
+
 
 
