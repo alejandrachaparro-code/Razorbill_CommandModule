a83f099e2ba9e943e1273d5f757147ec4507bca6
Sarah Schwarz <sarahschwarza@gmail.com>
2025-11-06 17:33:11 -0800

    Change serial resource from ASRL8 to ASRL11
    Updated resource identifiers for serial connection on D2.

--- checkResources.py
+++ checkResources.py
@@ -7,14 +7,19 @@ print(rm)
 print(rm.list_resources())
 
 
+#D1
+#inst = rm.open_resource('ASRL8::INSTR')
+#insta = rm.open_resource("GPIB0::28::INSTR")
 
-inst = rm.open_resource('ASRL8::INSTR')
+#D2
+inst = rm.open_resource('ASRL11::INSTR')
 insta = rm.open_resource("GPIB0::28::INSTR")
 
 print("razorbill:")
 print(inst.query("*IDN?"))
 print("cap bridge:")
 print(insta.query("*IDN?"))
+
 
 
 
